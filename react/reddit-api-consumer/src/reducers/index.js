cc2eea42b30a8c88c7d1564c108d1f5f46014689
João Fernandes <ojoaofernandes@gmail.com>
2018-10-19 14:29:50 -0300

    (React/reddit-api-consumer): Add an initial state to post list.

--- react/reddit-api-consumer/src/reducers/index.js
+++ react/reddit-api-consumer/src/reducers/index.js
@@ -1,4 +1,12 @@
-const initialState = {};
+const initialState = {
+  posts: [
+    { id: 'a', title: 'Test' },
+    { id: 'b', title: 'Test' },
+    { id: 'c', title: 'Test' },
+    { id: 'f', title: 'Test' },
+    { id: 'e', title: 'Test' },
+  ],
+};
 
 function rootReducer(state = initialState, action) {
   return state;
