f3e20480d972d4f6a0da5f52a79060d3d44bacff
João Fernandes <ojoaofernandes@gmail.com>
2018-10-19 14:03:49 -0300

    (React/reddit-api-consumer): Add a simple markup with mocked data.

--- react/reddit-api-consumer/src/components/App/index.js
+++ react/reddit-api-consumer/src/components/App/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
+const currentTopic = 'Microservices';
+const fetchedTopics = ['JavaScript', 'Apache Kafka', 'Docker'];
+const items = [
+  { id: 'a', title: 'Test' },
+  { id: 'b', title: 'Test' },
+  { id: 'c', title: 'Test' },
+  { id: 'f', title: 'Test' },
+  { id: 'e', title: 'Test' },
+];
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="app">
+      <h1>Reddit API Consumer</h1>
+
+      <div>
+        <h2>Search</h2>
+        <input type="text" placeholder="Search..." />
+
+        <div>
+          Current Topic:
+          {currentTopic}
+          <button type="button">Refresh</button>
+        </div>
+
+        <div>
+          Jump to:
+          {fetchedTopics.join(' | ')}
+        </div>
+      </div>
+
+      <div>
+        <h2>Posts</h2>
+        <ul>
+          {items.map(i => <li key={i.id}>{i.title}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
